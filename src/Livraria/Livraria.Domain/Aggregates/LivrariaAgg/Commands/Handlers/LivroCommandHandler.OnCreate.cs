42b68feb60c6d98c62dcaf23a88d978b67cb5c06
felipepassionniutech <168574882+felipepassionniutech@users.noreply.github.com>
2024-12-02 23:31:37 -0300

    Fixing missing models

---
+++ src/Livraria/Livraria.Domain/Aggregates/LivrariaAgg/Commands/Handlers/LivroCommandHandler.OnCreate.cs
@@ -0,0 +1,25 @@
+﻿namespace Niu.Nutri.Livraria.Domain.Aggregates.LivrariaAgg.Commands.Handlers
+{
+    using Entities;
+    using Microsoft.Extensions.DependencyInjection;
+    using Niu.Nutri.Core.Application.DTO.Http.Models.CommonAgg.Commands.Responses;
+    using Niu.Nutri.Livraria.Domain.Aggregates.LivrariaAgg.Repositories;
+    using System.Threading.Tasks;
+
+    public partial class LivroCommandHandler : BaseLivrariaAggCommandHandler<Livro>
+    {
+        public async override Task<DomainResponse> OnCreateAsync(Livro entity)
+        {
+            var assuntoRepo = _serviceProvider.GetRequiredService<IAssuntoRepository>();
+            var autorRepo = _serviceProvider.GetRequiredService<IAutorRepository>();
+
+            var autoresIds = entity.Autores.Select(x => x.Id);
+            var assuntosIds = entity.Assuntos.Select(x => x.Id);
+
+            entity.Autores = (await autorRepo.SelectAllAsync(filter: x => autoresIds.Contains(x.Id), selector: x => x)).ToList();
+            entity.Assuntos = (await assuntoRepo.SelectAllAsync(filter: x => assuntosIds.Contains(x.Id), selector: x => x)).ToList();
+
+            return await base.OnCreateAsync(entity);
+        }
+    }
+}
